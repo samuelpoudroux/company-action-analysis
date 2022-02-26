408b555ef71c5d0cffd94380d681f2f2d416bf09
samuelpoudroux <samuel.poudroux.com>
2022-02-26 22:57:53 +0100

    add log

--- services/morningStar/morningStar.js
+++ services/morningStar/morningStar.js
@@ -16,17 +16,25 @@ const {
 } = require('./utils');
 
 async function getMorningStarData(companyName) {
+  console.log('openBrowser')
   const browser = await openBrowser();
   const page = await browser.newPage();
   await goToPage(MORNING_STAR_URL, page);
   await acceptCookies(page);
   await searchActionByName(page, companyName);
+  console.log('getActionName')
   const name = await getActionName(page);
+  console.log('getActionPrice')
   const price = await getActionPrice(page);
+  console.log('getActionVolume')
   const volume = await getActionVolume(page);
+  console.log("getMarketCapitalisation")
   const marketCapitalization = await getMarketCapitalisation(page);
+  console.log("getIncomeStatement")
   const incomeStatement = await getIncomeStatement(page);
+  console.log("getBalanceSheet")
   const balanceSheet = await getBalanceSheet(page);
+  console.log("getCashFlow")
   const cashFlow = await getCashFlow(page);
   await closeBrowser(browser);
   return {
