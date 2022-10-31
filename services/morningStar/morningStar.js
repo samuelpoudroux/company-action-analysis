3096adbea31efa309f34a4d6362542b48d5d72c0
samuelpoudroux <samuel.poudroux.com>
2022-10-31 19:14:23 +0100

    temporary mock

--- services/morningStar/morningStar.js
+++ services/morningStar/morningStar.js
@@ -15,6 +15,8 @@ const {
   getCashFlow,
   getAllRatios,
 } = require("./utils");
+
+const mock = require('./airLiquide.json')
 const NodeCache = require("node-cache");
 
 const cache = new NodeCache();
@@ -33,54 +35,54 @@ async function getMorningStarData(companyName) {
     let cashFlow;
     let ratios;
     console.log("openBrowser");
-    if (!cache.keys().find((e) => e.includes(companyName))) {
-      browser = await openBrowser();
-      page = await browser.newPage();
-      await goToPage(MORNING_STAR_URL, page);
-      await acceptCookies(page);
-      await searchActionByName(page, companyName);
-      console.log("getActionName");
-      name = await getActionName(page);
-      console.log("getActionPrice");
-      price = await getActionPrice(page);
-      console.log("getActionVolume");
-      volume = await getActionVolume(page);
-      console.log("getMarketCapitalisation");
-      marketCapitalization = await getMarketCapitalisation(page);
-      console.log("getIncomeStatement");
-      incomeStatement = await getIncomeStatement(page, cache, companyName);
-      console.log("getBalanceSheet");
-      balanceSheet = await getBalanceSheet(page, cache, companyName);
-      console.log("getCashFlow");
-      cashFlow = await getCashFlow(page, cache, companyName);
-      console.log("getKeyRatios");
-      keyRatios = await getKeyRatios(page, cache, companyName);
-      ratios = getAllRatios({
-        price,
-        volume,
-        marketCapitalization,
-        incomeStatement,
-        balanceSheet,
-        cashFlow,
-      });
-    }
-    console.log("getAllRatios");
-    
+    // if (!cache.keys().find((e) => e.includes(companyName))) {
+    //   browser = await openBrowser();
+    //   page = await browser.newPage();
+    //   await goToPage(MORNING_STAR_URL, page);
+    //   await acceptCookies(page);
+    //   await searchActionByName(page, companyName);
+    //   console.log("getActionName");
+    //   name = await getActionName(page);
+    //   console.log("getActionPrice");
+    //   price = await getActionPrice(page);
+    //   console.log("getActionVolume");
+    //   volume = await getActionVolume(page);
+    //   console.log("getMarketCapitalisation");
+    //   marketCapitalization = await getMarketCapitalisation(page);
+    // }
+    // console.log("getIncomeStatement");
+    // incomeStatement = await getIncomeStatement(page, cache, companyName);
+    // console.log("getBalanceSheet");
+    // balanceSheet = await getBalanceSheet(page, cache, companyName);
+    // console.log("getCashFlow");
+    // cashFlow = await getCashFlow(page, cache, companyName);
+    // console.log("getKeyRatios");
+    // keyRatios = await getKeyRatios(page, cache, companyName);
+    // console.log("getAllRatios");
+    // ratios = getAllRatios({
+    //   price,
+    //   volume,
+    //   marketCapitalization,
+    //   incomeStatement,
+    //   balanceSheet,
+    //   cashFlow,
+    // });
 
-    if (!cache.keys().find((e) => e.includes(companyName))) {
-      await closeBrowser(browser);
-    }
-    return {
-      name,
-      price,
-      volume,
-      marketCapitalization,
-      incomeStatement,
-      balanceSheet,
-      cashFlow,
-      keyRatios,
-      ratios,
-    };
+    // if (!cache.keys().find((e) => e.includes(companyName))) {
+    //   await closeBrowser(browser);
+    // }
+    // return {
+    //   name,
+    //   price,
+    //   volume,
+    //   marketCapitalization,
+    //   incomeStatement,
+    //   balanceSheet,
+    //   cashFlow,
+    //   keyRatios,
+    //   ratios,
+    // };
+    return mock
   } catch (error) {
     console.log("getMorningStarData", error);
   }
